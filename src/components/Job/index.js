0187a94685ba123644967c1729dec728db4cdcf6
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-08-19 16:42:52 -0400

    css styles

---
+++ src/components/Job/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Job = () => (
+    <div className="job-component">
+        <div className="job-title">CEO and Director</div>
+        <div className="job-company">Self-Improvement</div>
+        <div className="job-years">16 years</div>
+    </div>
+)
+
+export default Job;
--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ResumeHeader from '../../components/ResumeHeader';
+import Job from '../../components/Job';
 
 const Resume = () => (
     <div className="view-container resume-page">
         <h3>Resume Page</h3>
         <ResumeHeader />
+        <hr />
+        <Job />
     </div>
 )
 
