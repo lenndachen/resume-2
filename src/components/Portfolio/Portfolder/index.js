0dfb9e8193d918a9280ab5b4e9a3b893aa2f0b6a
Lennda Chen <lindachen548@gmail.com>
2019-09-05 16:13:38 -0400

    4:13 9.5.2019

--- src/components/Portfolio/Portfolder/index.js
+++ src/components/Portfolio/Portfolder/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-function Portfolio(props) {
-    return (
+const Portfolder = () => (
       
-      <span className="portfolio-component" onClick={props.onClick}>
-        {props.value}
-        <a className="portfolio-1" href="../../containers/TicTacToe/index.js">TicTacToe</a>
-        <br />
-        <a className="portfolio-2" href="../../containers/calculator/index.js">Calculator</a>
-        <br />
-        <a className="portfolio-3" href="../../containers/Social-Card/index.js"> Social Card</a>
-        <br/>
-        <a className="portfolio-4" href="../../containers/Resume/index.js"> Resume</a>
-      </span>
-    )
-}
+ <div className="portfolio-container">
 
-export default Portfolio;
+        <div className="portfolio-tictactoe" href="../../containers/TicTacToe/index.js">TicTacToe </div>
+        
+        <div className="portfolio-calculator" href="../../containers/calculator/index.js">Calculator</div>
+   
+        <div className="portfolio-socialcard" href="../../containers/Social-Card/index.js"> Social Card</div>
+    
+        <div className="portfolio-resume" href="../../containers/Resume/index.js"> Resume</div>
+</div>
+    
+
+)
+
+export default Portfolder;
