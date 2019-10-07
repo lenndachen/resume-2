73dc835ec014062eb65b81b996abd56082405f31
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-10-07 14:15:33 -0400

    finished resume

--- src/components/Resume/Main/index.js
+++ src/components/Resume/Main/index.js
@@ -3,20 +3,21 @@ import Logo from '../../../assets/images/Capture.PNG';
 
 const Main = (props) => (
     <div className="main-component">
-        <div>
-        <h4 className="main-name">{props.main.Name}</h4>
-        <img className="logo" src={Logo} alt="logo" />
-        </div>
-        <br />
-        <div className="main-initial-component">
-
-        </div>
-        <br />   
-        <div>
-        <li className="main-address">{props.main.Address}</li>
-        <li className="main-email">{props.main.Email}</li>
-        <li className="main-github" href="https://github.com/lenndachen" >{props.main.Github}</li>
-        <li className="main-linkedin" href="https://www.linkedin.com/in/linda-chen-6b713198/">{props.main.LinkedIn}</li>
+        <div className="mainnamelogo">
+            <div className="namediv">
+                <h4 className="main-name">{props.main.Name}</h4>
+            </div>
+            <div className="logodiv">
+                <img className="logo" src={Logo} alt="logo" />
+            </div>
+        </div> 
+        <div className="infoonme">
+            <li className="main-address">{props.main.Address}</li>
+            <div className="notincludingaddress">
+                <li className="main-email">{props.main.Email}</li>
+                <li className="main-github" href="https://github.com/lenndachen" >{props.main.Github}</li>
+                <li className="main-linkedin" href="https://www.linkedin.com/in/linda-chen-6b713198/">{props.main.LinkedIn}</li>
+            </div>
         </div>
     </div>
 )
--- src/containers/Resume/index.js
+++ src/containers/Resume/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Experiences from '../../components/Resume/Experiences';
-import Educations from '../../components/Resume/Educations';
-import Skills from '../../components/Resume/Skills';
-import Mains from '../../components/Resume/MainMap';
-import Growths from '../../components/Resume/Growths';
-import Extracurricular from '../../components/Resume/Extracurricula';
-import Achievements from '../../components/Resume/Achievements';
-
+import React from "react";
+import Experiences from "../../components/Resume/Experiences";
+import Educations from "../../components/Resume/Educations";
+import Skills from "../../components/Resume/Skills";
+import Mains from "../../components/Resume/MainMap";
+import Growths from "../../components/Resume/Growths";
+import Extracurricular from "../../components/Resume/Extracurricula";
+import Achievements from "../../components/Resume/Achievements";
 
 class Resume extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading:false,
+      loading: false,
       main: [],
       skill: [],
       experience: [],
@@ -35,149 +34,144 @@ class Resume extends React.Component {
   }
 
   getMain() {
-    const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Main?maxRecords=3&view=Grid%20view" 
-    fetch( 
-      url,
-      {
-        headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-      }
-    )
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('Mains', responseData);
-      const main = responseData.records;
-      this.setState({ main }, () => {
-
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Main?maxRecords=3&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("Mains", responseData);
+        const main = responseData.records;
+        this.setState({ main }, () => {});
       });
-  });
- }
- getGrowth() {
-  const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Growth?maxRecords=3&view=Grid%20view" 
-  fetch( 
-    url,
-    {
-      headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-    }
-  )
-  .then(response => response.json())
-  .then(responseData => {
-    console.log('Growths', responseData);
-    const growth = responseData.records;
-    this.setState({ growth }, () => {
-
-    });
- });
- }
+  }
+  getGrowth() {
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Growth?maxRecords=3&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("Growths", responseData);
+        const growth = responseData.records;
+        this.setState({ growth }, () => {});
+      });
+  }
 
   getEducation() {
-    const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Education?maxRecords=3&view=Grid%20view" 
-    fetch( 
-      url,
-      {
-        headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-      }
-    )
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('All Education', responseData);
-      const education = responseData.records;
-      this.setState({ education }, () => {
-
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Education?maxRecords=3&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("All Education", responseData);
+        const education = responseData.records;
+        this.setState({ education }, () => {});
       });
-  });
- }
-   getExperience() {
-     const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Experience?maxRecords=30&view=Grid%20view" 
-     fetch( 
-       url,
-       {
-         headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-       }
-     )
-  
-          .then(response => response.json())
-          .then(responseData => {
-          console.log('My Experience', responseData);
-          const experience = responseData.records;
-          this.setState({ experience }, () => {
-
-     });
-  
-  });
-    
   }
-   getSkill() {
-     const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Skill?maxRecords=15&view=Grid%20view" 
-     fetch( 
-      url,
-     {
-         headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-       }
-     )
+  getExperience() {
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Experience?maxRecords=30&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("My Experience", responseData);
+        const experience = responseData.records;
+        this.setState({ experience }, () => {});
+      });
+  }
+  getSkill() {
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Skill?maxRecords=15&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("My skills", responseData);
+        const skill = responseData.records;
+        this.setState({ skill }, () => {});
+      });
+  }
 
-     .then(response => response.json())
-     .then(responseData => {
-       console.log('My skills', responseData);
-       const skill = responseData.records;
-       this.setState({ skill }, () => {
+  getExtracurricular() {
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Extracurricular?maxRecords=15&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("My skills", responseData);
+        const extracurricular = responseData.records;
+        this.setState({ extracurricular }, () => {});
+      });
+  }
 
-       });
-   });
- }
-
- getExtracurricular() {
-  const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Extracurricular?maxRecords=15&view=Grid%20view" 
-  fetch( 
-   url,
-  {
-      headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-    }
-  )
-
-  .then(response => response.json())
-  .then(responseData => {
-    console.log('My skills', responseData);
-    const extracurricular = responseData.records;
-    this.setState({ extracurricular }, () => {
-
-    });
-});
-}
-
-getAchievement() {
-  const url = "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Achievement?maxRecords=15&view=Grid%20view" 
-  fetch( 
-   url,
-  {
-      headers : { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
-    }
-  )
-
-  .then(response => response.json())
-  .then(responseData => {
-    console.log('My skills', responseData);
-    const achievement = responseData.records;
-    this.setState({ achievement }, () => {
-
-    });
-});
-}
-
+  getAchievement() {
+    const url =
+      "https://api.airtable.com/v0/appIxVtdy2R9iES5U/Achievement?maxRecords=15&view=Grid%20view";
+    fetch(url, {
+      headers: { Authorization: "Bearer " + process.env.REACT_APP_AIRTABLE_KEY }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("My skills", responseData);
+        const achievement = responseData.records;
+        this.setState({ achievement }, () => {});
+      });
+  }
 
   render() {
     return (
-      <div className="container"> 
-        <span className="container-main"><Mains main={this.state.main} /></span>
-        <span className="container1"> <h1>Growths </h1><Growths growth={this.state.growth}/> </span>
-        <span className="container1"> <h1>Skills </h1><Skills skill={this.state.skill} /> </span>
-        <span className="container1"><h1>Experience</h1><Experiences experience={this.state.experience} /> </span>
-        <span className="container1"><h1>Education</h1><Educations education={this.state.education} /> </span>
-        <span className="container1"><h1>Achievements</h1><Achievements achievement={this.state.achievement} /> </span>
-        <span className="container1"><h1>Extracurriculars</h1><Extracurricular extracurricular={this.state.extracurricular} /> </span>
+      <div className="portfolio-grid">
+        
+      <div className="left-column">
+        <div className="container-main">
+            <Mains main={this.state.main} />
+        </div>
+        <div className="experience-container">
+            <h1>Experience</h1>
+            <Experiences experience={this.state.experience} />
+        </div>
+        <div className="extracurricular-container">
+            <h1>Extracurriculars</h1>
+            <Extracurricular extracurricular={this.state.extracurricular} />
+        </div>
       </div>
-    )
+        
+         <div className="right-column">
+          <div className="skills-container">
+            <h1>Skills </h1>
+            <Skills skill={this.state.skill} />
+          </div>
+          <div className="education-container">
+            <h1>Education</h1>
+            <Educations education={this.state.education} />
+          </div>
+        <div className="achievement-container">
+            <h1>Achievements</h1>
+            <Achievements achievement={this.state.achievement} />
+          </div>
+          <div className="growth-container">
+            <h1>Growths </h1>
+            <Growths growth={this.state.growth} />
+          </div>
+        </div>
+        
+   
+        
+       
+       
+      </div>
+    );
   }
 }
-
 
 export default Resume;
